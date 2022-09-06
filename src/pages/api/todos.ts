d4da86032bcb0859eb47c3ee07ab3d6202dbfa91
Funes Avila Cesar de Jesus <fuunesto@gmail.com>
2022-09-06 16:54:01 -0500

    :

--- src/pages/api/todos.ts
+++ src/pages/api/todos.ts
@@ -3,6 +3,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     // get all todos
